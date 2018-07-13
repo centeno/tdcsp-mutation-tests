8ea01f6c92fcb3d61f7a3d3d93f9a333808c6f29
Ricardo Gaete Duarte <ricardo.gaete@latam.com>
2018-07-13 16:48:08 -0400

    First Commit: Skeleton generated with Spring initialzr

---
+++ src/main/java/com/latam/mutation/ExampleApp/ExampleAppApplication.java
@@ -0,0 +1,12 @@
+package com.latam.mutation.ExampleApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleAppApplication.class, args);
+	}
+}
